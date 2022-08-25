1f37021e7706c4c362d676753fad03898a9e4c9d
Daniel Igwe <daniel.igwe000@gmail.com>
2022-08-25 10:20:12 +0000

    corrected code for negative case

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         number = -1 * number
-        ld = number % 10
-        print("{:d}".format(-1 *ld), end="")
-        return (-1 * ld)
+        print("{:d}".format(number % 10), end="")
+        return (number % 10)
     else:
         print("{:d}".format(number % 10), end="")
         return (number % 10)
