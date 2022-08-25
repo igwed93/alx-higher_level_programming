73eedec31d15c0620645a2ea81b1ec2a59a77fa0
Daniel Igwe <daniel.igwe000@gmail.com>
2022-08-25 09:53:24 +0000

    Included two new files

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    for i in range(ord('a'), ord('z') + 1):
+        if i == ord(c):
+            return True
+        else:
+            continue
+    return False
