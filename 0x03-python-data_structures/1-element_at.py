21eae93518bca03e4b246f9eb85bdb80b725b198
Daniel Igwe <daniel.igwe000@gmail.com>
2022-08-29 22:28:33 +0000

    Included: 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 def element_at(my_list, idx):
-    if idx < 0 | idx > len(my_list):
-        return None
-    else:
+    if 0 <= idx < len(my_list):
         return (my_list[idx])
+    else:
+        return None
