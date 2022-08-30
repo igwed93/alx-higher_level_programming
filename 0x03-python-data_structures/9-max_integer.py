d6a6fa13d8759cddd7a5379d6937ca512031b3ec
Daniel Igwe <daniel.igwe000@gmail.com>
2022-08-30 01:16:51 +0000

    Added: 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        MAX = 0
+        for n in my_list:
+            if n > MAX:
+                MAX = n
+        return MAX
+    else:
+        return None
