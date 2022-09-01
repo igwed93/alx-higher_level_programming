71b83d416dc8343f0b576b12c79be932b810b4f9
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-01 19:07:57 +0000

    Added: 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    r_str = roman_string
+    if type(r_str) != str:
+        return 0
+    elif not r_str:
+        return 0
+    num = 0
+    sum = 0
+    roman_numerals = {"M": 1000, "D": 500, "C": 100, "XL": 40, "L": 50, "IX":
+                      9, "X": 10, "IV": 4, "V": 5, "II": 2, "III": 3, "I": 1}
+    while r_str != "":
+        for i in r_str:
+            j = r_str.index(i)
+            if i in roman_numerals:
+                if len(r_str) == 1:
+                    num = roman_numerals[i]
+                    sum += num
+                    return (sum)
+                elif i == "I" and r_str[j+1] == "X":
+                    i = "IX"
+                    r_str = r_str[:j+2] + r_str[j+2:]
+                elif i == "I" and r_str[j+1] == "V":
+                    i = "IV"
+                    r_str = r_str[:j+2] + r_str[j+2:]
+                num = roman_numerals[i]
+                sum += num
+                r_str = r_str[:j] + r_str[j+1:]
+    return (sum)
