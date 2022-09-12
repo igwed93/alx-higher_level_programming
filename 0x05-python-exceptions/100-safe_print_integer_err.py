6c4faa410f5ce94a49766bc6ad6a3606c4709e15
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-12 23:35:07 +0000

    Advanced task 1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
     except Exception as error:
-        print("Exception:", error, file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
     else:
         return True
