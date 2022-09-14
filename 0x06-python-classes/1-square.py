17779aaa8f64173d148ac93103691a80a8270b44
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-14 22:33:07 +0000

    Added: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a Class Square"""
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size):
+        """ Private instance attribute """
+        self.__size = size
+
+
+if __name__ == "___main__":
+    Square()
