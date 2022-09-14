66cbb5d0672d038a596d5f1b0b390c62e8ab6843
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-14 23:18:59 +0000

    Included: 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Define a Class Square"""
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size=0):
+        """ Private instance attribute """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ property getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ property setter """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Public intance method """
+        return self.__size ** 2
+
+
+if __name__ == "___main__":
+    Square()
