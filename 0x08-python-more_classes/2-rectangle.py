33735875f9d51edc22d8452d9b6adee1970c87b3
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-19 22:35:46 +0000

    Added: 2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""Define a Rectangle Class"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+    """
+    def __init__(self, width=0, height=0):
+        """ private instance attribute """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ property getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ property setter """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ property setter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ property setter """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        return (2 * (self.__width + self.__height))
+
+
+if __name__ == "__main__":
+    Rectangle()
