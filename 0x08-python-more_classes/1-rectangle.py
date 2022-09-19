8ef19737fe0a169b373a469746c539c5978e6e9d
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-19 22:16:39 +0000

    Added: 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = width
+            self.__width  = value
 
     @property
     def height(self):
@@ -33,12 +33,12 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ property setter """
-        elif type(value) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = height
+            self.__height = value
 
 
 if __name__ == "__main__":
