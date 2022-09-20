0bd13a4d62b1b33a4c43703cb1d2328efda0c40b
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-20 22:42:14 +0000

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(phrase=[]):
-    phrase += ["Bestschool"]
-    return ", ".join(phrase)
+def magic_string(text=[]):
+    text += ["Bestschool"]
+    return ", ".join(text)
