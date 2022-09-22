e52136b8b2e79e48f815ce2fc63ece6a88ad00b2
Daniel Igwe <daniel.igwe000@gmail.com>
2022-09-22 15:43:20 +0000

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that prints a square using #'s to the stdout
+"""
+
+def print_square(size):
+    """
+    prints a square with #'s
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float):
+        raise ValueError("size must be an integer")
+
+    if size == 0:
+        print(" ")
+    for index in range(size):
+        print("#" * size)
