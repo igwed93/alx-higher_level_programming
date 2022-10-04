9825b898e201e7e38d5faf0d33ec7a9d0a3ab91d
Daniel Igwe <daniel.igwe000@gmail.com>
2022-10-04 15:57:52 +0000

    Updated base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 create a class base that has a private class attribute __nb_objects
 and public instance attribute id
 """
+import json
 
 
 class Base:
@@ -12,6 +13,7 @@ class Base:
         id
     Methods:
         __init__()
+        to_json_string(list_dictionaries):
     """
     __nb_objects = 0  # public class attribute
 
@@ -22,6 +24,15 @@ class Base:
         else:  # else assign it to an increment of the public class attribute
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the json rep of list_dictionaries'''
+        if list_dictionaries is None:
+            return "[]"
+        elif list_dictionaries == [] or list_dictionaries == [{}]:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
 
 if __name__ == "__main__":
