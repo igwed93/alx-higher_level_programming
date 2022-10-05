68e468f6fee47aa8d2f994a98ce48f2e331da191
Daniel Igwe <daniel.igwe000@gmail.com>
2022-10-05 15:05:31 +0000

    Included: test_rectangle.py

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for Rectangle unit tests.'''
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from random import randrange
+from contextlib import redirect_stdout
+import io
