b19094cbfc7b610de572700d8fc12671335847cb
Daniel Igwe <daniel.igwe000@gmail.com>
2022-10-05 15:09:35 +0000

    Included: test_square.py

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''my Square unittests'''
+import unittest
+from models.base import Base
+from models.square import Square
+from random import randrange
+from contextlib import redirect_stdout
+import io
