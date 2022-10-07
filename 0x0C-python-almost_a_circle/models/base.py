f14ed47c4eedb74c6294224859f926c92baaf881
Daniel Igwe <daniel.igwe000@gmail.com>
2022-10-07 00:27:51 +0000

    Advanced task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,8 @@ class Base:
         from_json_string(json_string)
         create(cls, **dictionary)
         load_from_file(cls)
+        save_to_file_csv(cls, list_objs)
+        load_from_file_csv(cls)
     """
     __nb_objects = 0  # public class attribute
 
@@ -84,6 +86,68 @@ class Base:
         with open(file, "r", encoding="utf-8") as f:
             return [cls.create(**arg) for arg in
                     cls.from_json_string(f.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes in csv format
+            Args:
+                cls - filename
+                list_objs - object to convert
+        """
+        import csv
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls is None:
+            return
+        if list_objs is not None:
+            list_objs = [obj.to_dictionary() for obj in list_objs]
+        filename = "{}.csv".format(cls.__name__)  # create filename
+        with open(filename, mode="w", encoding="utf-8") as csv_file:
+            # open the file in csv format using the filename created
+            # create field names
+            if cls is Rectangle:
+                fieldnames = ["id", "width", "height", "x", "y"]
+            elif cls is Square:
+                fieldnames = ["id", "size", "x", "y"]
+            # write fieldnames into the csv file
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            # write each element of every dictionary under their
+            # respective columns
+            for dic in list_objs:
+                writer.writerow(dic)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ deserializes in csv format
+            Args:
+                cls - filename
+        """
+        import csv
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls is None:
+            return
+        filename = "{}.csv".format(cls.__name__)  # create filename
+        with open(filename, mode="r", encoding="utf-8") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 0
+            list_objs = []
+            attr = []
+            for row in csv_reader:
+                attr_dic = {}
+                if line_count == 0:
+                    for key in row:
+                        attr.append(key)
+                    line_count += 1
+                for key, value in row.items():
+                    attr_dic[key] = int(value)
+                line_count += 1
+                list_objs.append(cls.create(**attr_dic))
+            return list_objs
 
 
 if __name__ == "__main__":
