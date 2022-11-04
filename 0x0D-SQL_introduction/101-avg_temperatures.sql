1d3ce3e4c3a1991fd01f28a17d66978b4496f1b6
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-04 03:01:14 +0100

    Temperatures #0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- script that displays the average temperature in the file, temperatures.sql
 -- by city ordered by temperature (descending)
 
-SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_tmp DESC;
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
