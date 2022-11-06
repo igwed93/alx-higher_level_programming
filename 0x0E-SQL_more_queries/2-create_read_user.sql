0be3f22617a7e09fb9bf6b274c25c12fa1805196
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-06 17:38:10 +0100

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- script shouldn't fail if hbtn_0d_2 and user_0d_2 already exists
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
