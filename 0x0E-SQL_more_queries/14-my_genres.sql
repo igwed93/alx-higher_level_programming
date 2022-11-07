8dc42c19283420a2d8fb3add5608bdccc3c925fb
Daniel Igwe <daniel.igwe000@gmail.com>
2022-11-07 14:54:19 +0100

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,20 @@
+-- lists all genres of the show Dexter
+-- wget https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- run this first to import a SQL dump -->
+--      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+--      import the dump table into the hbtn_0d_tvshows
+--
+-- each record should display: tv_shows.title - tv_show_genres.genre_id
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+SELECT g.`name`
+    FROM tv_shows AS s
+INNER JOIN tv_show_genres AS t
+    ON s.`id` = t.`show_id`
+INNER JOIN tv_genres AS g
+    ON g.`id` = t.`genre_id`
+WHERE s.`title`='Dexter'
+GROUP BY g.`name`
+ORDER BY g.`name` ASC;
