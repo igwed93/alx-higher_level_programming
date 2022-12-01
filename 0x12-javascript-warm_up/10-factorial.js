42ca4fa1f33b20e8ca8ae75a7071492bae72e536
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-01 03:17:06 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* sript that computes and prints a factorial */
+
+const n = parseInt(process.argv[2]);
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(n));
