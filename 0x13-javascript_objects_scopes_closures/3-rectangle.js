e7f05e417cd811d8749bbfdfc058800e2a0f8e06
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-04 13:44:44 +0100

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+// Script that defines a class Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let str = 'X';
+    for (let j = 1; j < this.width; j++) {
+      str += 'X';
+    };
+
+    while (i < this.height) {
+      console.log(str);
+      i++;
+    }
+  }
+};
