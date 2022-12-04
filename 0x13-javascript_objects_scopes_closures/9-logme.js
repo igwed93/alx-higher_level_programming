6bfba456a7941f33277906a775ffe9271ca5ec65
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-04 18:44:57 +0100

    Log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// function that prints the number of arguments
+// already printed and the new argument value
+
+let count = -1;
+exports.logMe = function (item) {
+  count++;
+  console.log(`${count}: ${item}`);
+};
