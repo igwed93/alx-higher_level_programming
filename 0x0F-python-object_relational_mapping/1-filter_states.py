7a3ca0159b4033819bb7856accee6284088c6f8e
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-06 22:20:24 +0100

    Filter States4

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states that starts with N
+from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    # connect to the database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to execute quries with SQL
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
