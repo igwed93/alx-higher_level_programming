beeafc4e6a46f51d5265bf3ac0952d1d33a94e4a
Daniel O. Igwe <105175646+igwed93@users.noreply.github.com>
2022-12-16 20:22:32 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-#Bash script that takes in a URL, sends a request to the URL,
-#displays the size of the body of the response
-
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
 curl -sI "$1" | grep "Content-Length:" | cut -f 2 -d " "
