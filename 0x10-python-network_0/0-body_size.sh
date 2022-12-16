874225e76964bc841d3d0db5fd0c717de5fb5c36
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-16 20:19:45 +0100

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+#Bash script that takes in a URL, sends a request to the URL,
+#displays the size of the body of the response
+
+curl -sI "$1" | grep "Content-Length:" | cut -f 2 -d " "
