e0cdd250da789f6e9192b0edc2de36ce321bf596
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-17 21:53:14 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
-""" module that finds the peak in a list of unsorted integers """
+""" find a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
     """finds the peak value in a list"""
     if list_of_integers == [] or not (isinstance(list_of_integers, list)):
         return (None)
-    my_list = list_of_integers[:]
-    try:
-        my_list.sort()
-        length = len(my_list)
-    except TypeError:
-        return (None)
-    return (my_list[length - 1])
+    myList = list_of_integers[:]
+    first = 0
+    last = len(myList) - 1
+
+    if myList[first] > myList[first+1]:
+        return myList[first]
+    if myList[last] > myList[last-1]:
+        return myList[last]
+
+    mid = (first+last)//2
+    if myList[mid-1] < myList[mid] and myList[mid+1] < myList[mid]:
+        return myList[mid]
+    if myList[mid] < myList[mid-1]:
+        return find_peak(myList[first:mid+1])
+    elif myList[mid] < myList[mid+1]:
+        return find_peak(myList[mid:last+1])
+    else:
+        return myList[first]
