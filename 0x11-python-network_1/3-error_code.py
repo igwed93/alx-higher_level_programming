6bc8cc55aa4aafcfb94d0d7039dd35a5a2a258b1
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-20 13:06:53 +0100

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in a URL, sends request to it and displays
+the body of the response decoded in utf-8.
+HTTPError exceptions are well managed
+Usage: ./3-error_code.py http://0.0.0.0:5000
+"""
+
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    import urllib.error
+    from sys import argv
+
+    url = argv[1]
+    req = Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(reponse.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: ", e.code)
