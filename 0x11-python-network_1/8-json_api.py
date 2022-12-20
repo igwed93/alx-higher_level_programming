330bdcf60d8e6be4f9d4e0cccf72b730851808d3
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-20 16:19:30 +0100

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+Usage: ./8-josn_api.py letter
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = argv[1]
+    payload = {"q": letter}
+    r = requests.post(url, params=payload)
+
+    try:
+        dic = r.json()
+        if dic:
+            print("[{}] {}".format(dic.get('id'), dic.get('name')))
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
